172d7a01dccc3024818481eea271d37cf5f05ba4
Sean D Gillespie <sean@mistersg.net>
2024-10-04 21:46:24 -0400

    chore: Add a nix flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix }@attrs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ haskellNix.overlay ];
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+
+        project = pkgs.haskell-nix.cabalProject' {
+          src = ./.;
+          compiler-nix-name = "ghc96";
+
+          shell.tools = {
+            cabal = {};
+            fourmolu = {};
+            hlint = {};
+            haskell-language-server = {};
+          };
+
+          shell.buildInputs = with pkgs; [
+            nixpkgs-fmt
+          ];
+        };
+
+
+        flake = project.flake { };
+
+      in
+        pkgs.lib.recursiveUpdate flake {
+          packages.default = flake.packages."obloxious:exe:obloxious";
+        });
+}
