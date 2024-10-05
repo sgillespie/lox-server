4ba1b1831b2987dcceb32afd5aabe003335be725
Sean D Gillespie <sean@mistersg.net>
2024-10-04 22:48:56 -0400

    build: Add IOG's nix resolvers

--- flake.nix
+++ flake.nix
@@ -39,4 +39,21 @@
         pkgs.lib.recursiveUpdate flake {
           packages.default = flake.packages."obloxious:exe:obloxious";
         });
+
+  nixConfig = {
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "sgillespie.cachix.org-1:Zgif/WHW2IzHqbMb1z56cMmV5tLAA+zW9d5iB5w/VU4="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://cache.iog.io"
+      "https://sgillespie.cachix.org"
+    ];
+
+    allow-import-from-derivation = "true";
+    experimental-features = ["nix-command flakes"];
+  };
 }
